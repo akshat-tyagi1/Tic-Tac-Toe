b1a919a663151b4c61568dfd48289c884f1608cb
Akshat Tyagi <akshat.tyagi1909@gmail.com>
2026-07-04 16:22:07 +0530

    add main game interface

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+const board = document.querySelector(".board");
+const name1 = document.querySelector(".name1");
+const name2 = document.querySelector(".name2");
+
+createBoard();
+
 function createPlayer(name, marker) {
-  return { name, marker };
+  let score = 0;
+  return { name, marker, score };
 }
 
 const player1 = createPlayer("Akshat", "X");
@@ -9,7 +16,7 @@ const Gameboard = (function () {
   let board = ["", "", "", "", "", "", "", "", ""];
 
   function addMark(index, marker) {
-    if (board[index] !== "") return;
+    if (board[index] !== "") return false;
     board[index] = marker;
     return true;
   }
@@ -20,36 +27,37 @@ const Gameboard = (function () {
     }
   }
 
-  function printBoard() {
-    console.log(board);
-  }
-
   function readBoard() {
     let boardCopy = board.slice();
     return boardCopy;
   }
 
-  return { addMark, resetBoard, readBoard, printBoard };
+  return { addMark, resetBoard, readBoard };
 })();
 
 const gameController = (function () {
-  let gameStatus = "";
+  let gameStatus = { status: null, winner: null };
   let activePlayer = player1;
 
   function playRound(index) {
-    if (gameStatus !== "") {
+    if (gameStatus.status !== null) {
       return;
     }
+
     let roundStatus = Gameboard.addMark(index, activePlayer.marker);
-    if (roundStatus) {
+
+    if(!roundStatus) return;
+
+    gameStatus = gameController.checkWinner(activePlayer);
+
+    if(gameStatus.status === "Win") return gameStatus.winner.score++;
+
+    if (gameStatus.status === null) {
       activePlayer = activePlayer === player1 ? player2 : player1;
     }
-    gameStatus = gameController.checkWinner();
-    // console.log(gameStatus);
-    console.log(Gameboard.readBoard());
   }
 
-  function checkWinner() {
+  function checkWinner(mover) {
     let gameArr = Gameboard.readBoard();
 
     const winningCombinations = [
@@ -71,17 +79,15 @@ const gameController = (function () {
         gameArr[b] === gameArr[c] &&
         gameArr[a] !== ""
       ) {
-        return gameArr[a] === player1.marker
-          ? `${player1.name} Won`
-          : `${player2.name} Won`;
+        return { status: "Win", winner: mover }
       }
     }
 
     if (!gameArr.includes("")) {
-      return "Tie";
+      return { status: "Tie", winner: null };
     }
 
-    return "";
+    return { status: null, winner: null};
   }
 
   function getActivePlayer() {
@@ -89,8 +95,22 @@ const gameController = (function () {
   }
 
   function getGameStatus() {
-    return gameStatus
+    return gameStatus;
   }
 
   return { playRound, checkWinner, getActivePlayer, getGameStatus };
 })();
+
+function createBoard() {
+  for (let i = 0; i < 9; i++) {
+    const button = document.createElement("button");
+    button.classList.add("cell");
+    button.dataset.index = i;
+    button.textContent = "X";
+    button.dataset.mark = "X"
+    board.appendChild(button);
+  }
+}
+
+name1.textContent = `${player1.name}: ${player1.score}`;
+name2.textContent = `${player2.name}: ${player2.score}`;
